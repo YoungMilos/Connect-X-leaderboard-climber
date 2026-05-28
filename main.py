e18fc8e3c22a087b5f2dc4cfba3f97f8fd0778e6
hoanglax <hoangpptter84@gmail.com>
2026-05-28 21:42:35 +0700

    init: project structure and imports

---
+++ main.py
@@ -0,0 +1,13 @@
+import os
+import subprocess
+import ctypes
+import random
+import time
+import array
+import base64
+import zlib
+import json
+
+# ==============================================================================
+# PART 1: C++ PERFECT SOLVER
+# ==============================================================================
